843509fa21dd944e37d294ae91bb929875992f9e
Johannes Rudolph <johannes.rudolph@gmail.com>
2018-08-27 22:55:23 +0200

    RepositoryManager methods now return Iterator

--- src/main/scala/net/virtualvoid/fotofinish/Main.scala
+++ src/main/scala/net/virtualvoid/fotofinish/Main.scala
@@ -213,20 +213,21 @@ class RepositoryManager(val config: RepositoryConfig) {
   val FileNamePattern = """^[0-9a-f]{128}$""".r
   import Scanner._
 
-  def allRepoFiles: Iterable[FileInfo] =
+  def allRepoFiles(): Iterator[FileInfo] =
     Scanner.allFilesMatching(config.storageDir, byFileName(str => FileNamePattern.findFirstMatchIn(str).isDefined))
+      .iterator
       .map(f => Hash.fromString(config.hashAlgorithm, f.getName))
       .map(config.fileInfoOf)
 
-  def allFiles: Iterable[FileAndMetadata] = {
-    allRepoFiles
+  def allFiles(): Iterator[FileAndMetadata] = {
+    allRepoFiles()
       .map { fileInfo =>
         FileAndMetadata(fileInfo, MetadataStore.load(fileInfo))
       }
   }
 
   lazy val inodeMap: Map[(Long, Long), FileInfo] = {
-    allRepoFiles
+    allRepoFiles()
       .map { info =>
         val UnixFileInfo(dev, ino) = Scanner.unixFileInfo(info.repoFile.toPath)
 
