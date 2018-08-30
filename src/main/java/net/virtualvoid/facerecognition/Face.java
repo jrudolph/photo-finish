047f88e687c0e713b74c6f6be004043d87a76cb5
Johannes Rudolph <johannes.rudolph@gmail.com>
2018-08-30 22:32:11 +0200

    Formatting syntax

--- src/main/java/net/virtualvoid/facerecognition/Face.java
+++ src/main/java/net/virtualvoid/facerecognition/Face.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Face extends Structure {
     public static class ByReference extends Face implements Structure.ByReference {}
 
-    public long left,top,right,bottom;
+    public long left, top, right, bottom;
     public float[] model = new float[128];
 
     protected List<String> getFieldOrder() {
