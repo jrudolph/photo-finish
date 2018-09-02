ede347c4e35b15d6330e51ffd3e371e351d0079c
Johannes Rudolph <johannes.rudolph@gmail.com>
2018-09-02 19:00:15 +0200

    Ingest multiple dirs at once

--- src/main/scala/net/virtualvoid/fotofinish/Main.scala
+++ src/main/scala/net/virtualvoid/fotofinish/Main.scala
@@ -21,19 +21,33 @@ object Settings {
   val manager = new RepositoryManager(repoConfig)
 }
 object MainScanner extends App {
+
   import Settings._
-  val dir = new File("/home/johannes/Fotos/2016")
-  println("Ingesting new files")
-  val infos = new Scanner(repoConfig, manager).scan(dir)
 
-  println("Updating basic metadata for ingested fotos")
-  infos.par.foreach(MetadataStore.updateMetadataFor(_, IngestionDataExtractor, repoConfig))
+  val dirs =
+    {
+      (2003 to 2020).map(year => s"/home/johannes/Fotos/$year") ++
+        Seq[String]()
+    }
+      .map(new File(_))
+      .filter(_.exists)
 
-  println("Updating exif metadata for ingested fotos")
-  infos.par.foreach(MetadataStore.updateMetadataFor(_, ExifBaseDataExtractor, repoConfig))
+  def ingestDir(dir: File): immutable.Seq[FileInfo] = {
+    println(s"Ingesting new files from $dir")
+    val infos = new Scanner(repoConfig, manager).scan(dir)
 
-  println("Updating all metadata for ingested fotos")
-  infos.par.foreach(MetadataStore.updateMetadata(_, repoConfig))
+    println("Updating basic metadata for ingested fotos")
+    infos.par.foreach(MetadataStore.updateMetadataFor(_, IngestionDataExtractor, repoConfig))
+
+    println("Updating exif metadata for ingested fotos")
+    infos.par.foreach(MetadataStore.updateMetadataFor(_, ExifBaseDataExtractor, repoConfig))
+
+    println("Updating all metadata for ingested fotos")
+    infos.par.foreach(MetadataStore.updateMetadata(_, repoConfig))
+
+    infos
+  }
+  dirs.flatMap(ingestDir)
 
   println("Updating remaining metadata")
   manager.allRepoFiles().toStream.par
