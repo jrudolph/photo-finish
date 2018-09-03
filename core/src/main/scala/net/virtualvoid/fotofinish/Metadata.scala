701eda8edccb7cca05e77f0a151eb326f36d7268
Johannes Rudolph <johannes.rudolph@gmail.com>
2018-09-03 15:03:36 +0200

    Provide OriginalFolders shortcut

--- core/src/main/scala/net/virtualvoid/fotofinish/Metadata.scala
+++ core/src/main/scala/net/virtualvoid/fotofinish/Metadata.scala
@@ -336,6 +336,7 @@ object MetadataShortcuts {
   val DateTaken = optional[ExifBaseData, DateTime](_.dateTaken)
   val CameraModel = optional[ExifBaseData, String](_.cameraModel)
   val OriginalFileNames = manyFromManyEntries[IngestionData, String](_.originalFileName)
+  val OriginalFolders = manyFromManyEntries[IngestionData, String](x => x.originalFilePath)
   val OriginalFullFilePaths = manyFromManyEntries[IngestionData, String](x => x.originalFullFilePath)
   val Thumbnail = optional[Thumbnail, ByteString](t => Some(t.data))
   val Faces = manyFromSingle[FaceData, FaceInfo](_.faces)
