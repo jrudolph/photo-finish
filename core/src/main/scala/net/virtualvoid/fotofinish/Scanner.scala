1f06ba271bf1b5b7ae5ab4ab8dc7f43fc25ccdaf
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-09-06 09:54:12 +0200

    index 3gp files

--- core/src/main/scala/net/virtualvoid/fotofinish/Scanner.scala
+++ core/src/main/scala/net/virtualvoid/fotofinish/Scanner.scala
@@ -97,7 +97,7 @@ class Scanner(config: RepositoryConfig) {
 }
 
 object Scanner {
-  val supportedFiles = withExtensions("jpg", "jpeg", "txt", "mp4", "avi", "mov")
+  val supportedFiles = withExtensions("jpg", "jpeg", "txt", "mp4", "avi", "mov", "3gp")
 
   def allFilesMatching(dir: File, fileFilter: FileFilter): Iterable[File] = {
     def iterate(dir: File): Iterator[File] = {
